9540ca10b86b4aa2e3547f67e4f133604fa9ee73
Kwabena Aduse-Poku <kwub@kwub-romero.local>
2023-08-15 17:14:39 -0400

    MHKB: add vial layout

--- keyboards/kwub/mhkb/keymaps/vial/keymap.c
+++ keyboards/kwub/mhkb/keymaps/vial/keymap.c
@@ -19,6 +19,24 @@
 enum keyboard_layers {
     _BL = 0,  // Base layer
     _F1,      // Function layer 1
-    _F2,      // Function layer 2
-    _F3       // Function layer 3
+    _F2       // Function layer 2
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // clang-format off
+    [_BL]= LAYOUT_ALL(
+        KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,             KC_ENT,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT,
+                 MO(_F1), KC_LGUI,          KC_BSPC,          KC_SPC,           KC_RALT, MO(_F2)),
+    [_F1]= LAYOUT_ALL(
+        KC_GRV,  KC_BSLS, KC_TRNS, KC_TRNS, KC_QUOT, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL,  KC_TRNS,
+        KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,             KC_0,
+        KC_TRNS,          KC_TRNS, KC_TRNS, KC_SCLN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_SLSH, KC_TRNS,
+                 KC_TRNS, KC_LCTL,          KC_TRNS,          KC_TRNS,          KC_RCTL, KC_TRNS),
+    [_F2]= LAYOUT_ALL(
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_END,  KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,          KC_TRNS,
+        KC_TRNS,          KC_TRNS, KC_VOLU, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_TRNS, KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_RGUI, KC_TRNS)
 };
